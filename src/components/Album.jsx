f4deca1bab41b25f4da60934efbd4000018f0c6c
Martín Colombo <colombopmartin@gmail.com>
2021-07-05 23:44:00 +0200

    Reverted mobileScrollSupport to true.

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -104,7 +104,7 @@ const Album = (props) => {
                   minHeight={album.height * album.sizeMultiplier / 2}
                   size="stretch"
                   maxShadowOpacity={0.5}
-                  mobileScrollSupport={false}
+                  mobileScrollSupport={true}
                   className="comic justify-self-center"
                   showCover={true}
                   clickEventForward={false}
